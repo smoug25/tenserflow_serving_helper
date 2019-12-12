a24378b60c6ba2a759df671237a4e70fdb00dc84
vorobyev_a <vorobyev_a@vorobyev.a-Z370M-DS3H>
2019-12-12 16:29:14 +0300

    fix import path

--- proto/build.sh
+++ proto/build.sh
@@ -2,19 +2,19 @@
 
 cd "$(dirname "$0")"
 
-for proto in $(find ./tensorflow -iname "*.proto"); do
+for proto in $(find ./github.com/smoug25/tensorflow_serving_helper/tensorflow -iname "*.proto"); do
 echo processing: $proto step 2
 protoc \
     -I . \
-    --go_out=plugins=grpc:../ \
+    --go_out=plugins=grpc:$GOPATH/src/ \
     $proto
 done
 
 #--ruby_out=plugins=grpc:../gen \
-for proto in $(find ./tensorflow_serving -iname "*.proto"); do
+for proto in $(find ./github.com/smoug25/tensorflow_serving_helper/tensorflow_serving -iname "*.proto"); do
 echo processing: $proto step 2
 protoc \
     -I . \
-    --go_out=plugins=grpc:../ \
+    --go_out=plugins=grpc:$GOPATH/src/ \
     $proto
 done
